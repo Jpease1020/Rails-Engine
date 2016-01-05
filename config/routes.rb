acce6b521d6d5f983c27768749ebb9b3f00267e1
Justin Pease <justinpease2@gmail.com>
2016-01-04 18:48:12 -0700

    merchants relationship complete

--- config/routes.rb
+++ config/routes.rb
@@ -36,11 +36,15 @@ Rails.application.routes.draw do
       end
 
       resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], module: "merchants", as: :items
+        resources :invoices, only: [:index], module: "merchants", as: :invoices
+
         collection do
           get 'find'
           get 'find_all'
           get 'random'
         end
+
       end
 
       resources :transactions, only: [:index, :show] do
