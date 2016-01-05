29b55cf10a714429a5039e9828168505d271878c
Justin Pease <justinpease2@gmail.com>
2016-01-04 20:34:46 -0700

    adds relationships for second level of project

--- app/controllers/api/v1/transactions/invoices_controller.rb
+++ app/controllers/api/v1/transactions/invoices_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::Transactions::InvoicesController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Invoice.where(transaction_id: params[:transaction_id])
+    respond_with Invoice.find_by(transaction_id: params[:transaction_id])
+    respond_with Transaction.find_by(params[:transaction_id]).invoice
+    respond_with Invoice.joins(:transactions).where
   end
 end
