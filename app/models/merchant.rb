1da59990cb0aa0446c690c63a61c177e1dfd6bb3
Justin Pease <justinpease2@gmail.com>
2016-01-06 17:52:57 -0700

    down to three failing spec harness tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,19 +4,24 @@ class Merchant < ActiveRecord::Base
   has_many :transactions, through: :invoices
 
   def self.revenue(id)
-    { "revenue" => joins(invoices: [:transactions, :invoice_items]).where("transactions.result = ? AND merchants.id = ?", "success", id).sum("invoice_items.quantity * invoice_items.unit_price")}
+    { "revenue" => joins(invoices: [:transactions, :invoice_items])
+                   .where("transactions.result = ? AND merchants.id = ?", "success", id)
+                   .sum("invoice_items.quantity * invoice_items.unit_price")}
   end
 
   def self.revenue_by_date(id, date)
-    { "revenue" => joins(invoices: [:transactions, :invoice_items]).where("transactions.result = ? AND merchants.id = ? AND invoices.created_at = ?", "success", id, date).sum("invoice_items.quantity * invoice_items.unit_price")}
+    { "revenue" => joins(invoices: [:transactions, :invoice_items])
+                   .where("transactions.result = ? AND merchants.id = ? AND invoices.created_at = ?", "success", id, date)
+                   .sum("invoice_items.quantity * invoice_items.unit_price")}
   end
 
   def self.all_merchants_revenue_by_date(date)
-    { "total_revenue" => joins(invoices: [:transactions, :invoice_items]).where("transactions.result = ? AND invoices.created_at = ?", "success", date).sum("invoice_items.quantity * invoice_items.unit_price")}
+    { "total_revenue" => joins(invoices: [:transactions, :invoice_items])
+                         .where("transactions.result = ? AND invoices.created_at = ?", "success", date)
+                         .sum("invoice_items.quantity * invoice_items.unit_price")}
   end
 
   def self.top_merchants_by_revenue(quantity)
-    ######## do i need to scope down to successfull transactions????????
     Merchant.select("merchants.*, count(invoice_items.unit_price * invoice_items.quantity) AS item_revenue")
             .joins(invoices: [:invoice_items, :transactions])
             .where("transactions.result = ?", "success")
@@ -33,8 +38,7 @@ class Merchant < ActiveRecord::Base
   end
 
   def favorite_customer
-    Customer.joins(:invoices)
-          .joins(:transactions)
+    Customer.joins(invoices: :transactions)
           .where("invoices.merchant_id = ? AND transactions.result = 'success'", id)
           .group('id')
           .order('count(invoices.customer_id) DESC')
